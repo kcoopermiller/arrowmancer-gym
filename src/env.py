1ee412f21d1ae1de81059e75d3a1985fff1e038b
Cooper Miller <kcoopermiller9@gmail.com>
2024-05-06 16:11:57 +0000

    added enemy, time steps, and adjusted reward accordingly

--- src/env.py
+++ src/env.py
@@ -10,30 +10,39 @@ class ArrowmancerEnv(gym.Env):
         self.grid_size = 3  # 3x3
         self.cell_size = 200  # 200x200 pixels
         self.screen = None
-        self.padding = 50 # Padding around the grid
+        self.padding = 75 # Padding around the grid
+        self.time_step = 0
+        self.enemy_attack_delay = 3  # Number of time steps before enemy attack activates
         self.units = units 
         self.num_units = len(units)
         self.unit_health = np.ones(self.num_units) # Health points for each unit
+        self.enemy_health = 1
         self.current_unit = 0  # Index of the current unit performing the dance
         self.current_move_index = 0  # Index of the current move in the dance pattern
 
-        self.action_space = spaces.Discrete(12)  # (Up, Right, Down, Left) x 3 Units
+        self.action_space = spaces.Discrete(15)  # (Up, Right, Down, Left, Attack) x 3 Units
         self.observation_space = spaces.Dict({
             'grid': spaces.Box(low=0, high=1, shape=(self.grid_size, self.grid_size), dtype=int),
             'unit_positions': spaces.Box(low=0, high=self.grid_size - 1, shape=(self.num_units, 2), dtype=int),
-            'enemy_attacks': spaces.Box(low=0, high=1, shape=(self.grid_size, self.grid_size), dtype=int),
+            'enemy_attacks': spaces.Box(low=0, high=3, shape=(self.grid_size, self.grid_size), dtype=int),
+            'time_step': spaces.Box(low=0, high=np.inf, shape=(1,), dtype=int),
             'current_unit': spaces.Discrete(self.num_units),
             'unit_health': spaces.Box(low=0, high=1, shape=(self.num_units,), dtype=float),
+            'enemy_health': spaces.Box(low=0, high=1, shape=(1,), dtype=float),
             'current_move_index': spaces.Discrete(6),
             'current_dance_pattern': spaces.Box(low=-13, high=13, shape=(6, 2), dtype=int)
         })
         self.reset()
 
     def reset(self):
+        self.time_step = 0
         # Reset the grid to an empty state
         self.grid = np.zeros((self.grid_size, self.grid_size), dtype=int)
         # Randomly initialize unit positions on the grid
         self.unit_positions = np.random.randint(0, self.grid_size, size=(self.num_units, 2))
+        # Reset health points
+        self.unit_health = np.ones(self.num_units) 
+        self.enemy_health = 1
         # Reset enemy attacks to an empty state
         self.enemy_attacks = np.zeros((self.grid_size, self.grid_size), dtype=int)
         self.current_unit = 0  # Reset the current unit to the first unit
@@ -41,70 +50,87 @@ class ArrowmancerEnv(gym.Env):
         return self._get_obs()
 
     def step(self, action):
-        unit = action // 4 # Choose the current unit to move based on the action
-        action = action % 4
+        unit = action // 5  # Choose the current unit to move based on the action
+        action = action % 5
         unit_pos = self.unit_positions[unit]
-        # Update the position of the current unit based on the action
-        if action == 0:  # Move up
-            new_pos = [unit_pos[0] - 1, unit_pos[1]]
-        elif action == 1:  # Move right
-            new_pos = [unit_pos[0], unit_pos[1] + 1]
-        elif action == 2:  # Move down
-            new_pos = [unit_pos[0] + 1, unit_pos[1]]
-        elif action == 3:  # Move left
-            new_pos = [unit_pos[0], unit_pos[1] - 1]
-
-        # Check if the new position is valid and update the unit's position
-        if self._is_valid_position(new_pos):
-            # Check if another unit is present at the new position
-            if self._is_unit_at_position(new_pos):  # TODO: Method is broken for this use case!! Fix it
-                # If so, swap the positions of the two units
-                # TODO: Should only be able to swap if adjacent to an edge
-                for i, pos in enumerate(self.unit_positions):
-                    if (pos == new_pos).all() and i != unit:
-                        self.unit_positions[i] = unit_pos
-                        self.unit_positions[unit] = new_pos
-                        break
-            else:
-                self.unit_positions[unit] = new_pos
-
-        # Check if the current unit's dance move is satisfied
         reward = 0
-        unit = self.units[self.current_unit]
-        move = dance_patterns[unit['name']][unit['level']][self.current_move_index]
-        if self._check_dance_move(move):
-            reward = 1 + 0.1 * self.current_move_index # 10% increase for combos, TODO: reset if the player attacks the enemy
-            self.current_move_index += 1
-            # Move to the next unit if the current unit has completed all dance moves
-            if self.current_move_index >= len(dance_patterns[unit['name']][unit['level']]):
-                self.current_unit = (self.current_unit + 1) % self.num_units
-                self.current_move_index = 0
 
-        # Generate enemy attacks targeting either one grid or a column of 3 grids with 20% probability
-        # TODO: This should be on a timer I believe and the units should have the ability to dodge
-        self.enemy_attacks = np.zeros((self.grid_size, self.grid_size), dtype=int)
-        attack = np.random.choice([0, 1], p=[0.8,0.2]) # 80% chance of no attack
-        attack_type = np.random.choice(['single', 'column'], p=[0.8, 0.2]) # 80% chance of single grid attack
+        if action < 4:  # Move action
+            # Update the position of the current unit based on the action
+            if action == 0:  # Move up
+                new_pos = [unit_pos[0] - 1, unit_pos[1]]
+            elif action == 1:  # Move right
+                new_pos = [unit_pos[0], unit_pos[1] + 1]
+            elif action == 2:  # Move down
+                new_pos = [unit_pos[0] + 1, unit_pos[1]]
+            elif action == 3:  # Move left
+                new_pos = [unit_pos[0], unit_pos[1] - 1]
+
+            # Check if the new position is valid and update the unit's position
+            if self._is_valid_position(new_pos):
+                # Check if another unit is present at the new position
+                if self._is_unit_at_position(new_pos):  # TODO: Method is broken for this use case!! Fix it
+                    # If so, swap the positions of the two units
+                    # TODO: Should only be able to swap if adjacent to an edge
+                    for i, pos in enumerate(self.unit_positions):
+                        if (pos == new_pos).all() and i != unit:
+                            self.unit_positions[i] = unit_pos
+                            self.unit_positions[unit] = new_pos
+                            break
+                else:
+                    self.unit_positions[unit] = new_pos
+
+            # Check if the current unit's dance move is satisfied
+            unit = self.units[self.current_unit]
+            move = dance_patterns[unit['name']][unit['level']][self.current_move_index]
+            if self._check_dance_move(move):
+                reward = 1 + 0.1 * self.current_move_index  # 10% increase for combos
+                self.current_move_index += 1
+                # Move to the next unit if the current unit has completed all dance moves
+                if self.current_move_index >= len(dance_patterns[unit['name']][unit['level']]):
+                    self.current_unit = (self.current_unit + 1) % self.num_units
+                    self.current_move_index = 0
+
+        else:  # Attack action
+            attack_strength = 0.05 + 0.02 * self.current_move_index  # 5% base strength + 2% increase for combos
+            self.enemy_health -= attack_strength
+            if self.enemy_health <= 0:
+                reward = 100 - self.time_step  # Higher reward for faster enemy defeat
+                terminated = True
+                return self._get_obs(), reward, terminated, False, {}
+            self.current_unit = (self.current_unit + 1) % self.num_units
+            self.current_move_index = 0
+
+        # Penalty for each time step
+        reward -= 0.1
+        self.time_step += 1
+
+        # Decrement the delay for existing enemy attacks
+        self.enemy_attacks = np.where(self.enemy_attacks > 0, self.enemy_attacks - 1, 0)        
+        # Generate enemy attacks targeting either one grid or a column of 3 grids
+        attack = np.random.choice([0, 1], p=[0.7, 0.3])  # 70% chance of no attack
+        attack_type = np.random.choice(['single', 'column'], p=[0.8, 0.2])  # 80% chance of single grid attack
         if attack:
             if attack_type == 'single':
                 # Attack a single random grid
                 attack_pos = tuple(np.random.randint(0, self.grid_size, size=2))
-                self.enemy_attacks[attack_pos] = 1
+                self.enemy_attacks[attack_pos] = self.enemy_attack_delay  # Set the delay as the attack value
             else:
                 # Attack a random column of 3 grids
                 attack_col = np.random.randint(0, self.grid_size)
-                self.enemy_attacks[:, attack_col] = 1
+                self.enemy_attacks[:, attack_col] = self.enemy_attack_delay
 
         # Check if units are hit by enemy attacks
-        # TODO: Add health points for units and decrement health points when hit
-        # TODO: I also need to create a win condition for the player where they have to defeat the enemy
         terminated = False
         truncated = False
         for i, pos in enumerate(self.unit_positions):
             if self.enemy_attacks[pos[0], pos[1]] == 1:
                 self.unit_health[i] -= 0.1  # Reduce unit health by 10%
+                reward -= 0.1  # Penalty for unit hit
+                self.enemy_attacks[pos[0], pos[1]] = 0 # Reset the enemy attack
                 if self.unit_health[i] <= 0:
                     terminated = True
+                    reward = -100  # Negative reward for unit defeat
                     break
 
         return self._get_obs(), reward, terminated, truncated, {}
@@ -124,11 +150,13 @@ class ArrowmancerEnv(gym.Env):
             'grid': self.grid,
             'unit_positions': self.unit_positions,
             'enemy_attacks': self.enemy_attacks,
+            'time_step': np.array([self.time_step]), # Convert scalar to numpy array for consistency
             'current_unit': self.current_unit,
             'unit_health': self.unit_health,
+            'enemy_health': np.array([self.enemy_health]), # Ditto
             'current_move_index': self.current_move_index,
             'current_dance_pattern': current_dance_pattern
-        }, 0 # dummy reward
+        }, 0 # Dummy reward
 
     def _check_dance_move(self, move):
         for pos in move:
@@ -182,16 +210,17 @@ class ArrowmancerEnv(gym.Env):
         if self.screen is None:
             pygame.init()
             screen_width = self.grid_size * self.cell_size + 2 * self.padding
-            screen_height = self.grid_size * self.cell_size + 2 * self.padding + 50 # Extra space for dance pattern info
+            screen_height = self.grid_size * self.cell_size + 2 * self.padding + 100  # Extra space for dance pattern info and enemy
             self.screen = pygame.display.set_mode((screen_width, screen_height))
             pygame.display.set_caption("Arrowmancer")
         
         self.screen.fill((130, 139, 115)) # Fill the screen with a light green color
 
         # Grid rendering
+        grid_top = self.padding + 100  # Add extra space above the grid
         for i in range(self.grid_size):
             for j in range(self.grid_size):
-                cell_rect = pygame.Rect(self.padding + j * self.cell_size, self.padding + i * self.cell_size, self.cell_size, self.cell_size)
+                cell_rect = pygame.Rect(self.padding + j * self.cell_size, grid_top + i * self.cell_size, self.cell_size, self.cell_size)
                 pygame.draw.rect(self.screen, (75, 68, 60), cell_rect)  # Draw cell background
                 pygame.draw.rect(self.screen, (130, 139, 115), cell_rect, 2)  # Draw cell border                
                 
@@ -199,7 +228,10 @@ class ArrowmancerEnv(gym.Env):
                 pink_square_size = int(self.cell_size * 0.8)
                 pink_square_rect = pygame.Rect(cell_rect.centerx - pink_square_size // 2, cell_rect.centery - pink_square_size // 2, pink_square_size, pink_square_size)
                 pygame.draw.rect(self.screen, (130, 98, 107), pink_square_rect, 5)
-                
+
+                if self.enemy_attacks[i, j] > 0:
+                    self._render_emoji("🟪", cell_rect.centerx, cell_rect.centery)
+
                 for k in range(self.num_units):
                     if (self.unit_positions[k] == [i, j]).all():
                         # If dancing unit use different emoji
@@ -214,8 +246,17 @@ class ArrowmancerEnv(gym.Env):
                         health_bar_fill_rect = pygame.Rect(health_bar_rect.left, health_bar_rect.top, int(health_bar_width * self.unit_health[k]), health_bar_height)
                         pygame.draw.rect(self.screen, (0, 255, 0), health_bar_fill_rect)
                         break
-                if self.enemy_attacks[i, j] == 1:
-                    self._render_emoji("🟪", cell_rect.centerx, cell_rect.centery)
+        
+        # Enemy rendering
+        enemy_x = (self.grid_size * self.cell_size) // 2 + self.padding
+        enemy_y = self.padding + 10 
+        self._render_emoji("🤓", enemy_x, enemy_y)
+        enemy_health_bar_width = 100
+        enemy_health_bar_height = 10
+        enemy_health_bar_rect = pygame.Rect(enemy_x - enemy_health_bar_width // 2, enemy_y + self.cell_size // 3 + 10, enemy_health_bar_width, enemy_health_bar_height)
+        pygame.draw.rect(self.screen, (255, 0, 0), enemy_health_bar_rect)
+        enemy_health_bar_fill_rect = pygame.Rect(enemy_health_bar_rect.left, enemy_health_bar_rect.top, int(enemy_health_bar_width * self.enemy_health), enemy_health_bar_height)
+        pygame.draw.rect(self.screen, (0, 255, 0), enemy_health_bar_fill_rect)
         
         # Dance pattern info rendering
         unit = self.units[self.current_unit]
@@ -229,7 +270,7 @@ class ArrowmancerEnv(gym.Env):
                 move_texts.append(font.render(f"{move}", True, (62, 80, 86)))
 
         name_text = font.render(f"{unit['name']} {unit['level']} Dance Pattern: ", True, (62, 80, 86))
-        name_rect = name_text.get_rect(x=self.padding, y=self.grid_size * self.cell_size + self.padding + 30)
+        name_rect = name_text.get_rect(x=self.padding, y=grid_top + self.grid_size * self.cell_size + 20) # Adjust the dance pattern info position
         self.screen.blit(name_text, name_rect)
 
         current_x = name_rect.right + 5
